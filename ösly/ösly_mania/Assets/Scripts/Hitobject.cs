53a7662da4e5c24d0c8e7e93062e9d2542b1ed1d
Lukas Hofwimmer <lukas.hofwimmer@gmail.com>
2018-01-25 10:57:05 +0100

    oslo osu! clone start commit, no beatmap processing

---
+++ ösly/ösly_mania/Assets/Scripts/Hitobject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitobject : MonoBehaviour {
+
+	public int lane { get; set;}
+	public int height { get; set;}
+	public int timefromstart { get; set;}
+	public int mode { get; set;} //
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ösly/ösly_mania/Assets/Scripts/InstantiaBeatmap.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+public class InstantiaBeatmap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		string beatmapInSystem = "C:\\Users\\User\\Documents\\Schule\\Programmieren\\Unity\\ösly\\ösly_mania\\Assets\\Beatmap\\363184 Spawn of Possession - Apparition\\Spawn of Possession - Apparition (XeoStyle) [Duty to God].osu";
+		var bmLines = GetAllFileLinesByType (beatmapInSystem);
+		GetComponent<AudioSource> ().Play ();
+		DrawBeatmap (bmLines);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public GameObject prefab;
+	public void DrawBeatmap (Dictionary<string, List<string>> beatmap)
+	{
+		GameObject note = Instantiate (prefab) as GameObject;
+
+		var notes = beatmap ["[HitObjects]"];
+
+		foreach (var line in notes)
+		{
+
+		}
+
+	}
+
+
+
+
+	public Dictionary<string, List<string>> GetAllFileLinesByType(string source)
+	{
+		string[] lines = File.ReadAllLines (source);
+
+		var dictionary = new Dictionary<string, List<string>> ();
+		string currentKey = "";
+		for(int i=0; i<lines.Length;i++)
+		{
+			
+
+
+			if(lines[i].Equals(""))
+			{
+				currentKey = lines[++i];
+				if(!dictionary.ContainsKey(currentKey))
+				{
+					dictionary [currentKey] = new List<string> ();
+				}
+			}
+			else
+			{
+				if(!dictionary.ContainsKey(currentKey))
+				{
+					dictionary [currentKey] = new List<string> ();
+				}
+				dictionary[currentKey].Add(lines[i]);
+				Console.WriteLine (lines[i]);
+			}
+
+		}
+		return dictionary;
+	}
+}
